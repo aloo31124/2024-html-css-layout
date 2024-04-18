9de354ca42b0e1ae2d5c3d21bc0f2d671dc59b83
DESKTOP-FP1235S\PC01 <aloo31124@gmail.com>
2024-04-18 17:21:30 +0800

    [形象網站] 上課 MrWebDesinger - coffee shop => 五, home 首頁 end + 六, about關於我們 + 七, menu菜單 start

--- MrWebDesinger/CoffeeShop/js/script.js
+++ MrWebDesinger/CoffeeShop/js/script.js
@@ -5,35 +5,43 @@ let cartItem = document.querySelector(".cart-items-container");
 let searchForm = document.querySelector(".search-form");
 
 /* 清除全部 icon active 效果 */
-function removeAllIconActive() {
+/* function removeAllIconActive() {
     navbar.classList.remove("active");
     cartItem.classList.remove("active");
     searchForm.classList.remove("active");
 }
-
+ */
 
 
 /* 導覽列切換 */
 document.querySelector("#menu-btn").onclick = () => {
-    removeAllIconActive();
+    cartItem.classList.remove("active");
+    searchForm.classList.remove("active");
+
     navbar.classList.toggle("active");
 }
 
 /* 商城列切換 */
 document.querySelector("#car-btn").onclick = () => {
-    removeAllIconActive();
+    navbar.classList.remove("active");
+    searchForm.classList.remove("active");
+
     cartItem.classList.toggle("active");
 }
 
 /* 搜尋外框切換 */
 document.querySelector("#search-btn").onclick = () => {
-    removeAllIconActive();
+    navbar.classList.remove("active");
+    cartItem.classList.remove("active");
+
     searchForm.classList.toggle("active");
 }
 
 
 window.scroll = () => {
-    removeAllIconActive();
+    navbar.classList.remove("active");
+    cartItem.classList.remove("active");
+    searchForm.classList.remove("active");
 }
 
 
