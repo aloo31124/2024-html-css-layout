6ba7483606d613602e5ad2a30805bd55389e4936
DESKTOP-FP1235S\PC01 <aloo31124@gmail.com>
2024-04-19 17:10:47 +0800

    [動畫效果] 研究滑鼠滾動 + 動畫呈現

---
+++ web-layout/scroll-show-animate/script.js
@@ -0,0 +1,20 @@
+
+/*
+    滑鼠滾動 on scroll 動畫效果
+*/
+const io = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.intersectionRatio > 0) {
+            entry.target.classList.add('this')
+        }
+    })
+})
+
+const box = document.querySelectorAll('.animate');
+
+box.forEach((el) => {
+    io.observe(el);
+})
+
+
+
