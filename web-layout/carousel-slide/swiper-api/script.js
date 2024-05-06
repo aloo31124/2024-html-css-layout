66c5a78631b32d9c269281057d462114ca5ac451
DESKTOP-FP1235S\PC01 <aloo31124@gmail.com>
2024-05-06 12:34:01 +0800

    [團隊優化] 第三方 => swaper輪播圖 研究1 => 操作 index

--- web-layout/carousel-slide/swiper-api/script.js
+++ web-layout/carousel-slide/swiper-api/script.js
@@ -1,14 +1,17 @@
 
 
-const swiper = new Swiper('.swiper', {
+let swiperTest = new Swiper('.swiper', {
   
     /* 輪流 */
-    loop: true,
+    /* loop: true, */
 
     /* 自動輪播 */
+/* 
     autoplay: {
       delay: 2000,
     },
+
+     */
     
     /* 方向: 水平:horizontal, 垂直:vertical  */
     direction: 'horizontal',
@@ -19,15 +22,35 @@ const swiper = new Swiper('.swiper', {
       clickable: true,
     },
   
-    // Navigation arrows
+    // 左右按鈕 (上/下一頁), css 綁定
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   
-    // And if we need scrollbar
+    // 下方進度 bar css 綁定
     scrollbar: {
       el: '.swiper-scrollbar',
     },
+});
+
+
+/* 
+
+  it文章:
+    https://ithelp.ithome.com.tw/articles/10321773
+  討論文章  
+    https://stackoverflow.com/questions/32945099/how-to-detect-current-slide-in-swiper-js
+ */
+function slideTab(index) {
+  console.log(index);
+  swiperTest.activeIndex = index;
+  //swiper.realIndex = index;
+}
+
+
+
+swiper.on('slideChange', function (e) {
+  // alert("slideChange e", e);  // e ==> event 無
 });
 
